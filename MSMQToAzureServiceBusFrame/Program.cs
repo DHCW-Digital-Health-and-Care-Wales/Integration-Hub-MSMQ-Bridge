0a3b0409009a07ceb8c726e4d0ec29714257b560
Muralidharan Jeyachandaran (DHCW - Software Development) <muralidharan.jeyachandaran@wales.nhs.uk>
2025-03-26 22:29:52 +0000

    Update code with closing queue connection

--- MSMQToAzureServiceBusFrame/Program.cs
+++ MSMQToAzureServiceBusFrame/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Messaging;
 using Azure.Messaging.ServiceBus;
+using System.Threading;
 using System.Threading.Tasks;
 using MSMQToAzureServiceBusFrame.Configuration;
 
@@ -8,58 +9,94 @@ namespace MSMQToAzureServiceBusFrame
 {
     class Program
     {
-       static async Task Main(string[] args)
+        // Add a static flag to control the shutdown process
+        private static bool _isShuttingDown = false;
+
+        static async Task Main(string[] args)
         {
             AppConfig config = null;
-            Console.WriteLine("Number of arguments: " + args.Length);
             if (args.Length > 0)
             {
-                 config =  AppConfig.ReadCommandLineArg(args);
-               
+                config = AppConfig.ReadCommandLineArg(args);
             }
             else
             {
-                 config = AppConfig.ReadEnvConfig();
-                Console.WriteLine("Connection String: " + config.ServiceBusConnectionString);
+                config = AppConfig.ReadEnvConfig();
             }
-                
+
             // Initialize the MSMQ queue
             using (MessageQueue msmqQueue = new MessageQueue(config.MsmqConnectionString))
             {
                 msmqQueue.Formatter = new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+                msmqQueue.MessageReadPropertyFilter.ArrivedTime = true;
 
                 var conn = config.ServiceBusConnectionString;
 
                 //// Initialize Service Bus client
                 var client = new ServiceBusClient(config.ServiceBusConnectionString);
-                var sender = client.CreateSender(config.ServiceBusQueueName);
+                var serviceBusSender = client.CreateSender(config.ServiceBusQueueName);  
 
                 Console.WriteLine("Starting to consume messages from MSMQ...");
 
-                while (true)
+                // Handle shutdown signals
+                AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
                 {
-                    try
+                    // Mark shutdown flag
+                    _isShuttingDown = true;
+                    Console.WriteLine("Shutting down gracefully...");
+                };
+
+                // Handling Ctrl+C
+                Console.CancelKeyPress += (sender, e) =>
+                {
+                    e.Cancel = true;  
+                    _isShuttingDown = true;  
+                    Console.WriteLine("Shutdown initiated...");
+                };
+
+                try
+                {
+                    while (!_isShuttingDown)
                     {
-                        // Receive message from MSMQ
-                        var msmqMessage = msmqQueue.Receive();
-
-                        if (msmqMessage != null)
+                        try
                         {
-                            // Process message (convert to string)
-                            string messageBody = msmqMessage.Body.ToString();
-                            
-                            // Create a Service Bus message
-                            var serviceBusMessage = new ServiceBusMessage(messageBody);
+                            // Receive message from MSMQ
+                            var msmqMessage = msmqQueue.Receive();
 
-                            // Send the message to Azure Service Bus
-                            await sender.SendMessageAsync(serviceBusMessage);
-                            Console.WriteLine("Message sent to Azure Service Bus.");
+                            if (msmqMessage != null)
+                            {
+                                // Process message (convert to string)
+                                string messageBody = msmqMessage.Body.ToString();
+
+                                DateTime arrivedTime = msmqMessage.ArrivedTime;
+
+                                // Create a Service Bus message
+                                var serviceBusMessage = new ServiceBusMessage(messageBody);
+
+                                serviceBusMessage.ApplicationProperties.Add("MSMQArrivedTime", arrivedTime);
+                                // Send the message to Azure Service Bus
+                                await serviceBusSender.SendMessageAsync(serviceBusMessage);  
+                                Console.WriteLine("Message sent to Azure Service Bus.");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+                            _isShuttingDown = true;
                         }
                     }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Error: {ex.Message}");
-                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Operation was canceled.");
+                    _isShuttingDown = true;
+                }
+                finally
+                {
+                    // Clean up
+                    await serviceBusSender.CloseAsync();  
+                    await client.DisposeAsync();
+                    Console.WriteLine("Service Bus sender and client closed.");
                 }
             }
         }
